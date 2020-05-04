5945113343251baab98cb78ad1a64336da2cba21
MarkTiukov <tyukov.mark@gmail.com>
2020-05-05 02:06:15 +0300

    Added a form and a data file for it

--- WebPy/app.py
+++ WebPy/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, make_response, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+
+path_to_data = "static/data.txt"
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("formPOST.html")
+
+
+@app.route('/', methods=["POST"])
+def getInfo():
+    with open(path_to_data, "a") as file:
+        file.write(request.form.get("Name"))
+        file.write("\n")
+    return make_response('Test worked!', 200)
 
 
 if __name__ == '__main__':
