c9b9ef2f74b7890f022b95a4b08c2989c19ba009
MarkTiukov <tyukov.mark@gmail.com>
2020-05-08 03:10:22 +0300

    Writes down data now

--- WebPy/app.py
+++ WebPy/app.py
@@ -4,6 +4,11 @@ app = Flask(__name__, template_folder="templates")
 
 path_to_data = "static/data.txt"
 
+available_fields_to_write = ["Name", "Email", "Message", "Field5", "Check1",
+                             "Check2", "Check3", "List"]
+
+current_number = 1
+
 
 @app.route('/')
 def hello_world():
@@ -12,9 +17,27 @@ def hello_world():
 
 @app.route('/', methods=["POST"])
 def getInfo():
+    global current_number
     with open(path_to_data, "a") as file:
-        file.write(request.form.get("Name"))
+        file.write("Data number " + str(current_number) + "\n")
+        for name in available_fields_to_write:
+            try:
+                print(request.form.get(name))
+                file.write(request.form.get(name).replace("\r\n", "\\n"))
+            except TypeError:
+                continue
+            except AttributeError:
+                print("here")
+                continue
+            except:
+                print(name)
+                file.write(" #$#DAMAGED DATA#$#\n")
+                return make_response(
+                    "Something went wrong writing your data. Please, try again",
+                    200)
+            file.write(" #&# ")
         file.write("\n")
+        current_number += 1
     return make_response('Test worked!', 200)
 
 
